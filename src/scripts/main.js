10d818abef886f7116911e536e3e0ee79e3ae466
Roma <romakoder@gmail.com>
2020-02-06 16:00:52 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-console.log('hello mate academy!');
+const thumbs = document.querySelector('#thumbs');
+const largeImg = document.querySelector('#largeImg');
+
+thumbs.onclick = function(e) {
+  const thumb = e.target.closest('.list-item__link');
+
+  if (!thumb) {
+    return;
+  }
+  showThumb(thumb.href, thumb.title);
+  event.preventDefault();
+};
+
+function showThumb(href, title) {
+  largeImg.src = href;
+  largeImg.alt = title;
+}
